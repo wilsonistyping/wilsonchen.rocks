e13aa72280a88b5642913ea67d42b611839194a3
KeroseneTrampolines <36680838+KeroseneTrampolines@users.noreply.github.com>
2020-04-11 16:27:46 -0400

    Update nutfactsbot.php

--- telegram/nutfactsbot.php
+++ telegram/nutfactsbot.php
@@ -1,11 +1,16 @@
-<?php
+<html>
+    <head>
+        <title>Nut Facts Bot PHP Interface</title>
+    </head>
+    <body>
+    <?php
 
-$botToken = "1161004001:AAFy9I35r8HWjIwuNY1OuYfA6S_VylGLS74";
-$website = "https://api.telegram.org/bot".$botToken;
+    $botToken = "1161004001:AAFy9I35r8HWjIwuNY1OuYfA6S_VylGLS74";
+    $website = "https://api.telegram.org/bot".$botToken;
 
-$update = file_get_contents($website."/getUpdates");
+    $update = file_get_contents($website."/getupdates");
 
-print_r($update);
-
-
-?>
+    print_r($update);
+    ?>
+    </body>
+</html>
